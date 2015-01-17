14c10618c9c6e3f198befb8a0a1d7af40653e883
RoboHawks <RoboHawks@10.0.1.84>
2015-01-17 13:57:28 -0500

    Initial test for Indexer control (Uses RobotDrive.tankDrive to contorl)

--- src/org/usfirst/frc/team3373/robot/Indexer.java
+++ src/org/usfirst/frc/team3373/robot/Indexer.java
@@ -1,5 +1,20 @@
 package org.usfirst.frc.team3373.robot;
 
-public class Indexer {
+import edu.wpi.first.wpilibj.RobotDrive;
 
+public class Indexer {
+	
+	RobotDrive indexer = new RobotDrive(0, 1);//using PWM ports 0 and 1 (using robotDrive to be able to use tank drive
+
+	public void indexControl(double leftY, double rightY){
+		
+		indexer.tankDrive(leftY, rightY);
+		
+	}
+	
+	
+	
+	
+	
+	
 }
