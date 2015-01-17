80add3fc9340602d83cb9437846030f74cd0a808
RoboHawks <RoboHawks@10.0.1.84>
2015-01-17 14:16:02 -0500

    changed name of wheel control method from indexControl to wheelControl

--- src/org/usfirst/frc/team3373/robot/Indexer.java
+++ src/org/usfirst/frc/team3373/robot/Indexer.java
@@ -4,9 +4,9 @@ import edu.wpi.first.wpilibj.RobotDrive;
 
 public class Indexer {
 	
-	RobotDrive indexer = new RobotDrive(0, 1);//using PWM ports 0 and 1 (using robotDrive to be able to use tank drive
+	RobotDrive indexer = new RobotDrive(0, 1);//using PWM ports 0 and 1 (using robotDrive to be able to use tank drive to control wheels)
 
-	public void indexControl(double leftY, double rightY){
+	public void wheelControl(double leftY, double rightY){
 		
 		indexer.tankDrive(leftY, rightY);
 		
