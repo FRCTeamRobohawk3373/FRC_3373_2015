a6f7ad8b0cfd3299c6088188baf9d2239dde5d9b
Philip Mulford <peragorefalith1@gmail.com>
2015-01-25 14:10:35 -0500

    First attempt at lifter code. It can't be this simple, can it?

--- src/org/usfirst/frc/team3373/robot/Lifter.java
+++ src/org/usfirst/frc/team3373/robot/Lifter.java
@@ -1,5 +1,51 @@
 package org.usfirst.frc.team3373.robot;
 
-public class Lifter {
+import edu.wpi.first.wpilibj.CANTalon;
 
+public class Lifter {
+	CANTalon leftActuator;
+	CANTalon rightActuator;
+	
+	double pr = 10;
+	double ir = 10;
+	double dr = 10;
+	
+	double pl = 10;
+	double il = 0;
+	double dl = 10;
+	
+	//difference in position in pot value
+	double offset = .05;
+	
+	/**
+	 * Initializes Lifter class, feeds in motor values to control lifting motors
+	 * @param leftActuatorID CANBus ID for left actuator
+	 * @param rightActuatorID CANBus ID for right actuator
+	 */
+	public Lifter(int leftActuatorID, int rightActuatorID){
+		leftActuator = new CANTalon(leftActuatorID);
+		rightActuator = new CANTalon(rightActuatorID);
+	
+		//Sets PID
+		rightActuator.setPID(pr, ir, dr);
+		leftActuator.setPID(pl, il, dl);
+		
+		//Sets value to be fed to CANTalons (Position in this case, v -1 to 1 speed)
+		rightActuator.changeControlMode(CANTalon.ControlMode.Position);
+		leftActuator.changeControlMode(CANTalon.ControlMode.Position);
+		
+		//Sets main type of sensor available on the CANBus
+		rightActuator.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogPot);
+		leftActuator.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogPot);
+	}
+	/**
+	 * Given a target position, goes to the position
+	 * @param targetPos position from 0-5 volts the lifter needs to go to
+	 */
+	public void goToPosition(double targetPos){
+			rightActuator.set(targetPos);
+			leftActuator.set(targetPos -offset);
+	}
+	
+	
 }
