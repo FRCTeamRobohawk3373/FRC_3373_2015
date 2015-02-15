02cfcce261ade2dc3064073b6158f56651692a08
Philip Mulford <peragorefalith1@gmail.com>
2015-02-15 14:28:32 -0500

    Lifter done

--- src/org/usfirst/frc/team3373/robot/Lifter.java
+++ src/org/usfirst/frc/team3373/robot/Lifter.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3373.robot;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Lifter {
 	CANTalon leftActuator;//currently ID 5
@@ -32,15 +33,23 @@ public class Lifter {
 	//distance between lift joint and actuator joint on arm
 	double jointThetaA = 24;
 	
-	double potScalar = 5;
+	double potScalarR = 783;
+	double potScalarL = 763;
 	double casingLength = 12;
-	
-	double maxPotValueR;
-	double minPotValueR;
-	double maxPotValueL;
-	double minPotValueL;
+	//Right Actuator
+	double maxPotValueR = 870;
+	double minPotValueR = 142;
+	double maxLengthR = 11.1875;//in inches
+	double minLengthR = 1.75;//in inches
+	//left Actuator
+	double maxPotValueL = 839;
+	double minPotValueL = 136;
+	double maxLengthL = 11;//in inches
+	double minLengthL = 1.8125;//in inches
 	
 	double diffBetweenPots = 0;
+	
+	double lifterTarget;
 	
 	//Have to use these as a work around to the PID controller class
 	PIDOutputObject leftActPIDOutput = new PIDOutputObject();
@@ -51,9 +60,9 @@ public class Lifter {
 	PIDInputObject rightActPIDInput = new PIDInputObject();
 	PIDInputObject errorPIDInput = new PIDInputObject();
 	
-	PIDController leftActPID = new PIDController(1, 0, 0, leftActPIDInput, leftActPIDOutput);
-	PIDController rightActPID = new PIDController(1, 0, 0, rightActPIDInput, rightActPIDOutput);
-	PIDController errorPID = new PIDController(1, 0, 0, errorPIDInput, errorPIDOutput);
+	PIDController leftActPID = new PIDController(.5, 0, 0, leftActPIDInput, leftActPIDOutput);
+	PIDController rightActPID = new PIDController(.5, 0, 0, rightActPIDInput, rightActPIDOutput);
+	PIDController errorPID = new PIDController(3, 0, 0, errorPIDInput, errorPIDOutput);
 	
 	/**
 	 * Initializes Lifter class, feeds in motor values to control lifting motors
@@ -82,6 +91,8 @@ public class Lifter {
 		leftActuator.changeControlMode(CANTalon.ControlMode.PercentVbus);
 		rightActuator.changeControlMode(CANTalon.ControlMode.PercentVbus);
 		
+		lifterTarget = getLeftActuatorLength();
+		
 		initPIDControllers();
 	}
 	
@@ -100,42 +111,40 @@ public class Lifter {
 	 * @param targetHeight target lifter height above the ground
 	 * @return target pot position (0-5V)
 	 */
-	/*
+	
 	public double heightToPosition(double targetHeight){
 		double actuatorLength;
 		cosTheta = (robotHeight - targetHeight)/armLength;
 		actuatorLength = Math.sqrt(Math.pow(jointThetaH, 2) + Math.pow(jointThetaA, 2) - (2*jointThetaH*jointThetaA*cosTheta));
 		
-		return actuatorLengthToPot(actuatorLength);
-	}*/
+		return actuatorLengthToPot(actuatorLength, potScalarR);
+	}
 	
 	/**
 	 * given a target actuator length, returns a pot position
 	 * @param actuatorTargetLength target length for to hit a certain lifter height
 	 * @return encoder position (0-5)
 	 */
-	/*
-	private double actuatorLengthToPot(double actuatorTargetLength){
+	
+	private double actuatorLengthToPot(double actuatorTargetLength, double potScalar){
 		double potPosition;
 		potPosition = (actuatorTargetLength-casingLength) * (potScalar/casingLength);
 		
 		return potPosition;
 	}
-	*/
+	
+	private double potValueToActuatorLength(double potValue, double potScalar){
+	
+		double actLength;
+		actLength = (potValue) * (casingLength/potScalar);
+	
+		return actLength;
+	}
 	
 	/*
 	 * Jamie's Code
 	 */
 	
-	public void relateActuators(){
-		double diffAtMax = maxPotValueR - maxPotValueL;
-		double diffAtMin = minPotValueR - minPotValueL;
-		double difference;
-		
-		if(diffAtMax == diffAtMin){
-			difference = diffAtMax;
-		}
-	}
 	
 	public double getLeftPotValue(){
 		return leftActuator.getAnalogInPosition();
@@ -147,32 +156,95 @@ public class Lifter {
 	}
 	
 	public void initPIDControllers(){
+		leftActPID.enable();
+		rightActPID.enable();
+		errorPID.enable();
 		//set input range
-		leftActPID.setInputRange(minPotValueR, maxPotValueL);
-		rightActPID.setInputRange(minPotValueR, maxPotValueR);
+		leftActPID.setInputRange(minLengthL, maxLengthL);
+		rightActPID.setInputRange(minLengthR, maxLengthR);
 		//set output range
 		leftActPID.setOutputRange(-0.5, 0.5);
 		rightActPID.setOutputRange(-0.5, 0.5);
-		errorPID.setOutputRange(-0.25, 0.25);
+		errorPID.setInputRange(0, 1);
+		errorPID.setOutputRange(-0.1, 0.1);
 		errorPID.setSetpoint(0);
+		
+		leftActPID.setPercentTolerance(5);
+		rightActPID.setPercentTolerance(5);
+		errorPID.setPercentTolerance(100);
 		//update PIDs
 		updatePIDControllers();
 	}
 	
 	public void updatePIDControllers(){
-		leftActPIDInput.setValue(getLeftPotValue());
-		rightActPIDInput.setValue(getRightPotValue());
-		errorPIDInput.setValue(getLeftPotValue() - getRightPotValue());
+		leftActPIDInput.setValue(getLeftActuatorLength());
+		//SmartDashboard.putNumber("Current Length", leftActuator.getAnalogInPosition());
+		//SmartDashboard.putNumber("CurrentPos", leftActuator.getAnalogInPosition());
+		rightActPIDInput.setValue(getRightActuatorLength());
+		errorPIDInput.setValue(getLeftActuatorLength() - getRightActuatorLength());
+	}
+	
+	public double getLeftActuatorLength(){
+		double slope = (maxLengthL - minLengthL)/(maxPotValueL - minPotValueL);
+		double potValue = leftActuator.getAnalogInPosition();
+		double length;
+		length = slope * (potValue - maxPotValueL) + maxLengthL;
+		System.out.println("Pot Value: "+ potValue);
+		return length;
+	}
+	public double getRightActuatorLength(){
+		double slope = (maxLengthR - minLengthR)/(maxPotValueR - minPotValueR);
+		double potValue = rightActuator.getAnalogInPosition();
+		double length;
+		length = slope * (potValue - maxPotValueR) + maxLengthR;
+		return length;
+	}
+	public double inchesToPotValueL(double length){
+		double slope = (maxPotValueR - minPotValueR)/(maxLengthR - minLengthR);
+		double correspondingPotValue;
+		correspondingPotValue = slope * (length - maxLengthR) + maxPotValueL;
+		return correspondingPotValue;
+	}
+	
+	/*public void extendLeft(double target){
+		if(Math.abs(target-leftActuator.getAnalogInPosition()) > 2){
+			updatePIDControllers();
+			leftActPID.setSetpoint(target);//inchesToPotValueL(target));
+			SmartDashboard.putNumber("Target: ", target);//inchesToPotValueL(target));
+			SmartDashboard.putNumber("Speed of Left Talon", leftActPIDOutput.getPIDValue());
+			leftActuator.set(leftActPIDOutput.getPIDValue());
+		}
+	}*/
+	
+	public void changeTarget(double target){
+		lifterTarget = target;
 	}
 	
 	
-	public void goToPosition(double targetPosition){//in potentiometer units
-		updatePIDControllers();
-		//set setpoint for Pid loops
-		leftActPID.setSetpoint(targetPosition);
-		rightActPID.setSetpoint(targetPosition);
-		//set motors to desired speed
-		leftActuator.set(leftActPIDOutput.getPIDValue() - errorPIDOutput.getPIDValue());
-		rightActuator.set(rightActPIDOutput.getPIDValue() + errorPIDOutput.getPIDValue());
+	public void goToLength(){//in inches
+		//if((Math.abs(lifterTarget - getLeftActuatorLength()) > .15) || (Math.abs(lifterTarget - getRightActuatorLength()) > .15)){
+			updatePIDControllers();
+			//set setpoint for Pid loops
+			leftActPID.setSetpoint(lifterTarget);
+			rightActPID.setSetpoint(lifterTarget);
+		
+			SmartDashboard.putNumber("Target: ", lifterTarget);
+			System.out.println("CurrentLeft" + getLeftActuatorLength());
+			
+		
+			SmartDashboard.putNumber("Left Current Length: ", getLeftActuatorLength());
+			SmartDashboard.putNumber("Right Current Length: ", getRightActuatorLength());
+		
+			SmartDashboard.putNumber("PID Output Left: ", leftActPIDOutput.getPIDValue());
+			SmartDashboard.putNumber("PID Output Right: ", rightActPIDOutput.getPIDValue());
+			SmartDashboard.putNumber("PID Output Error: ", errorPIDOutput.getPIDValue());
+		
+			//set motors to desired speed
+			leftActuator.set(leftActPIDOutput.getPIDValue());// + errorPIDOutput.getPIDValue());
+			SmartDashboard.putNumber("Left Actuator Speed: ", leftActuator.get());
+			rightActuator.set(rightActPIDOutput.getPIDValue());// - errorPIDOutput.getPIDValue());
+			SmartDashboard.putNumber("Right Actuator Speed: ", rightActuator.get());//.getPIDValue() + errorPIDOutput.getPIDValue());
+		//}
+
 	}
 }
